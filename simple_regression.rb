2e1e2121bb991605dfaeb5e9a0b3d5e82de68997
bendouglasweb <frulaa@yahoo.com>
2014-09-20 00:17:51 -0400

    First commit on scripts

---
+++ simple_regression.rb
@@ -0,0 +1,33 @@
+=begin
+  Maintainer(s)
+    Ben Douglas
+
+
+  Details
+    Development Stage : Development / Broken
+    Ruby version : 1.9.3
+    Date last updated : 9/19/2014
+
+  method : [method name]
+
+      Description
+        [method description]
+
+      Inputs
+        1st : [name used as argument]
+          [description of what's expected]
+
+
+      Outputs
+        Console
+          [Expected outputs in console. Possibly none]
+        Return
+          [Expected return value. Possibly none]
+        Harddisk
+          [Expected file and contents. Possibly none]
+=end
+
+# Pull in our simple gen
+require 'generators/simple_gen.rb'
+
+
---
+++ templates/ruby_starting_template.rb
@@ -0,0 +1,28 @@
+=begin
+  Maintainer(s)
+    [Primary maintainer]
+    [Secondary maintainer]
+
+  Details
+    Development Stage : [Development / Broken / Production]
+    Ruby version : [version]
+    Date last updated : [date]
+
+  method : [method name]
+
+      Description
+        [method description]
+
+      Inputs
+        1st : [name used as argument]
+          [description of what's expected]
+
+
+      Outputs
+        Console
+          [Expected outputs in console. Possibly none]
+        Return
+          [Expected return value. Possibly none]
+        Harddisk
+          [Expected file and contents. Possibly none]
+=end
